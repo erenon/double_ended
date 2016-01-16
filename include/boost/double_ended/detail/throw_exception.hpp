b7544755b3e109fc5d2c8ce8a0042d8c24d4744f
erenon <erenon2@gmail.com>
2016-01-16 14:39:02 +0100

    Increment copyright year

--- include/boost/double_ended/detail/throw_exception.hpp
+++ include/boost/double_ended/detail/throw_exception.hpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////////
 //
-// (C) Copyright Benedek Thaler 2015-2015. Distributed under the Boost
+// (C) Copyright Benedek Thaler 2015-2016. Distributed under the Boost
 // Software License, Version 1.0. (See accompanying file
 // LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
